9ddbc33db896c541853aaf821fe958d63ac82ea6
iasonLesg <73752572+iasonLesg@users.noreply.github.com>
2025-07-14 09:41:20 +0300

    Updated Everything
    Updated Everything

--- MedicalAssistant/obj/Release/net8.0-windows/MedicalAssistant.AssemblyInfo.vb
+++ MedicalAssistant/obj/Release/net8.0-windows/MedicalAssistant.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("MedicalAssistant"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6786175e2b6c88af7d1345004d9a6aa5c1ba7d1e"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d43c69b8f11564bd465373670bd92cd03025ca6"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("MedicalAssistant"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("MedicalAssistant"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
